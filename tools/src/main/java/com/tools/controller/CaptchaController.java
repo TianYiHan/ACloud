09630359bcfd91e878b48debcb12e60cdc4318ae
韩天一 <1395459722@qq.com>
2020-04-16 16:46:41 +0800

    日报2020.4.16 今日新增了redis模块，更新工具类，增加了log日志模块 下次更新计划： 完善集成redis，完善log日志

--- tools/src/main/java/com/tools/controller/CaptchaController.java
+++ tools/src/main/java/com/tools/controller/CaptchaController.java
@@ -1,9 +1,11 @@
 package com.tools.controller;
 import com.tools.entity.CaptchaUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,15 +19,19 @@ import java.io.IOException;
  * about:获取验证码接口
  */
 @RestController
+@Slf4j
 public class CaptchaController {
 
     @RequestMapping("getCaptcha")
     public void getCaptcha(HttpServletRequest req, HttpServletResponse resp)throws IOException {
+
+
         HttpSession session = req.getSession();
         String sessionId = session.getId();
         //获取redis连接
         Jedis jedis = new Jedis("106.14.208.219",6379);
         //查看服务是否运行
+
         System.out.println("服务正在运行: "+jedis.ping());
         jedis.set(sessionId,sessionId);
         jedis.expire(sessionId,10);//设置sessionId过期时间》秒
